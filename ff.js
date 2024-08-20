2101f7286a5c77c06b6de2f506d4b7ec964db037
JJunngx <136897299+JJunngx@users.noreply.github.com>
2024-08-20 18:00:27 +0700

    Delete ff.js

--- ff.js
+++
@@ -1,5 +0,0 @@
-const arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
-const n = 2;
-const item = [7, 8, [9, 10, 11], 12];
-
-console.log(...item.map((subItem) => [subItem, n - 1]));
